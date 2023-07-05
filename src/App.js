4324a4a3392593deb2e6690812219a82bde27cc6
Hammad Hussain <hammadasmat@gmail.com>
2023-07-05 15:53:44 +0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import {Video,Thumb} from "./components/Video";
-function App(){
-  return (
-  <div>
-      <div>Hello</div>
-      <Video></Video> 
-      <Thumb></Thumb>
-  </div> 
-  )
+import './App.css';
+import Video from './components/Video';
 
+function App() {
+  let obj = {
+    title: 'React JS tutorial',
+    views: '999K',
+    time: '1 year ago',
+    channel: 'Coder Dost',
+  };
+  return (
+    <div className="App">
+      <div>Videos</div>
+      <Video {...obj}></Video>
+      <Video title="Node JS tutorial" views="100K" time="1 month ago"></Video>
+      <Video
+        title="Mongo DB tutorial"
+        views="1M"
+        time="1 month ago"
+        channel="Coder Dost"
+      ></Video>
+    </div>
+  );
 }
+
 export default App;
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,9 +1,20 @@
-function Video(){
-    return <div>Video</div>
+import './Video.css';
 
+function Video({title,channel="Coder Dost",views,time}) {
+  return (
+      <>
+      <div className='container'>
+      <div className="pic">
+      <img src="http://placeimg.com/160/90/1" alt="Katherine Johnson" />
+      </div>
+      <div className="title">{title}</div>
+      <div className="channel">{channel}</div>
+      <div className="views">
+        {views} views <span>.</span> {time}
+      </div>
+      </div>
+      </>
+  );
 }
 
-function Thumb(){
-    return <div>Thumb</div>
-}
-export  {Video,Thumb};
+export default Video;
