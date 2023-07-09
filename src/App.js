49a50c82260cfc4aa0c53d3403f8dc0a5112cd30
Hammad Hussain <hammadasmat@gmail.com>
2023-07-09 15:01:47 +0500

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
+import { useState } from 'react';
 import './App.css';
-import Video from './components/Video';
-import videos from './data/data'
+import AddVideo from './components/AddVideo';
+import videoDB from './data/data';
+import VideoList from './components/VideoList';
 function App() {
-  
+  console.log('render App')
+
+  const [videos,setVideos] = useState(videoDB);
+  const [editableVideo,setEditableVideo] = useState(null);
+
+  function addVideos(video){
+      setVideos([
+            ...videos,
+            {...video, id: videos.length+1}
+          ]);
+  }
+  function deleteVideo(id){
+    setVideos(videos.filter(video=>video.id!==id))
+  }
+  function editVideo(id){
+    setEditableVideo(videos.find(video=>video.id===id))
+  }
+
+  function updateVideo(video){
+    const index = videos.findIndex(v=>v.id===video.id)
+    const newVideos = [...videos]
+    newVideos.splice(index,1,video)
+    setVideos(newVideos)
+  }
+
   return (
-    <div className="App">
-      <div>Videos</div>
-      {
-        videos.map(video=><Video
-          key={video.id}
-          title={video.title}
-          views={video.views}
-          time={video.time}
-          channel={video.channel}
-          verified={video.verified}
-          id={video.id}
-        ></Video>)
-      }
-    
+    <div className="App" onClick={()=>console.log('App')}>
+       <AddVideo addVideos={addVideos} updateVideo={updateVideo} editableVideo={editableVideo}></AddVideo>
+       <VideoList deleteVideo={deleteVideo} editVideo={editVideo}  videos={videos}></VideoList>
+
+
     </div>
   );
 }
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,11 +1,14 @@
 import './Video.css';
 
-function Video({title,id,channel="Coder Dost",views,time,verified}) {
-   
+function Video({title,id,channel="Coder Dost",views,time,verified,children,deleteVideo,editVideo}) {
+  console.log('render Video')
+
   
   return (
       <>
       <div className='container'>
+      <button className='close' onClick={()=>deleteVideo(id)}>X</button>  
+      <button className='edit' onClick={()=>editVideo(id)}>Edit</button>  
       <div className="pic">
       <img src={`https://picsum.photos/id/${id}/160/90`} alt="Katherine Johnson" />
       </div>
@@ -13,6 +16,9 @@ function Video({title,id,channel="Coder Dost",views,time,verified}) {
       <div className="channel">{channel} {verified && '✅'} </div>
       <div className="views">
         {views} views <span>.</span> {time}
+      </div>
+      <div>
+        {children}
       </div>
       </div>
       </>
